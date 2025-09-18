705e6093dc9be5f29f4a89236dd4d23e9487dca4
Taahirah Denmark <126827805+Engineernoob@users.noreply.github.com>
2025-09-18 17:25:36 -0500

    allow access to the wasm-core/pkg folder outside of verifier-ext

--- web/verifier-ext/vite.config.ts
+++ web/verifier-ext/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+
 export default defineConfig({
   server: {
-    host: '127.0.0.1',
-    port: 5173
-  }
+    host: "127.0.0.1",
+    port: 5173,
+    fs: {
+      // allow access to the wasm-core/pkg folder outside of verifier-ext
+      allow: ["..", "../wasm-core/pkg"],
+    },
+  },
 });
